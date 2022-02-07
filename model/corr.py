8549b326f06156f6e97f7092b407fb2ac957bf3c
LavenderLA <648347655@qq.com>
2022-02-07 09:52:31 +0800

    deformable voxel feature

--- model/corr.py
+++ model/corr.py
@@ -11,11 +11,17 @@ class CorrBlock(nn.Module):
         self.num_levels = num_levels
         self.resolution = resolution  # local resolution
         self.base_scale = base_scale  # search (base_sclae * resolution)^3 cube
-        self.voxel_out = nn.Sequential(
-            nn.Conv1d((self.resolution ** 3) * self.num_levels, 128, 1),
+        self.out_conv = nn.Sequential(
+            nn.Conv1d((self.resolution ** 3) * (self.num_levels + 1), 128, 1),
             nn.GroupNorm(8, 128),
             nn.PReLU(),
             nn.Conv1d(128, 64, 1)
+        )
+        self.voxel_conv = nn.Sequential(
+            nn.Conv2d(4, 32, 1),
+            nn.GroupNorm(8, 32),
+            nn.PReLU(),
+            nn.Conv2d(32, 1, 1)
         )
         
         self.knn = knn
@@ -51,8 +57,11 @@ class CorrBlock(nn.Module):
 
     def get_voxel_feature(self, coords):
         b, n_p, _ = coords.size()
+        b, n1, n2, _ = self.truncate_xyz2.shape
         corr_feature = []
-        from torch_scatter import scatter_add
+        base = self.get_base().to(coords.device)
+        num = self.resolution ** 3
+        from torch_scatter import scatter_add, scatter_max
         for i in range(self.num_levels):
             with torch.no_grad():
                 r = self.base_scale * (2 ** i)
@@ -75,7 +84,49 @@ class CorrBlock(nn.Module):
 
             corr_feature.append(corr.transpose(1, 2).contiguous())
 
-        return self.voxel_out(torch.cat(corr_feature, dim=1))
+        with torch.no_grad():
+            r = self.base_scale * (2 ** 3)
+            R = 2 * r
+            dis_voxel = torch.round((self.truncate_xyz2 - coords.unsqueeze(dim=-2)) / R)
+            valid_center_candidate = (torch.abs(dis_voxel) <= 1.5).all(dim=-1)
+            dis_voxel_intpos = dis_voxel - (-1)
+            center_candidates_idx = dis_voxel_intpos[:, :, :, 0] * (self.resolution ** 2) +\
+                dis_voxel_intpos[:, :, :, 1] * self.resolution + dis_voxel_intpos[:, :, :, 2]
+            center_candidates_idx = (center_candidates_idx.type(torch.int64) + 1) * valid_center_candidate
+
+            assert (center_candidates_idx < 0).nonzero().shape[0] == 0 and (center_candidates_idx >= 512).nonzero().shape[0] == 0, 'Out of scatter max bound!'
+            center_corr, center_idx = scatter_max(self.truncated_corr * valid_center_candidate, center_candidates_idx)
+            center_idx = center_idx[:, :, 1:]
+            mask_oob = ((center_idx >= n2) | (center_idx < 0) | (center_corr[:, :, 1:] <= 0))
+            center_idx[mask_oob] = 0
+            center_coord = torch.gather(self.truncate_xyz2, 2, center_idx.unsqueeze(dim=-1).expand(-1, -1, -1, 3))
+
+            idx_scatter = torch.ones((b, n1, n2), device=coords.device) * num
+            center_coords_move = []
+            for k in range(num):
+                center_coord_k = center_coord[:, :, k:k+1, :]
+                voxel_k_center = (coords + base[k].unsqueeze(dim=0).unsqueeze(dim=0) * R).unsqueeze(dim=2)
+                if center_coord_k.shape[2] == 0:
+                    center_coords_move.append(voxel_k_center)
+                    continue
+                center_coord_k_move = torch.clamp(center_coord_k - voxel_k_center, -(R-r) / 2, (R-r) / 2) + voxel_k_center
+                center_coords_move.append(center_coord_k_move)
+                dis_center = (self.truncate_xyz2 - center_coord_k_move) / r
+                replace = (torch.abs(dis_center) <= 0.5).all(dim=-1) & (~mask_oob[:, :, k:k+1])
+                idx_scatter[replace] = k
+
+            idx_scatter = idx_scatter.type(torch.int64).detach()
+            voxel_xyz = torch.cat(center_coords_move, dim=2)
+
+        assert (idx_scatter < 0).nonzero().shape[0] == 0 and (idx_scatter >= 512).nonzero().shape[0] == 0, 'Out of scatter add bound!'
+        corr_add = scatter_add(self.truncated_corr, idx_scatter)
+        corr_cnt = scatter_add(self.ones_matrix, idx_scatter)
+        voxel_corr = corr_add[:, :, :num] / torch.clamp(corr_cnt[:, :, :num], 1, n_p)
+        voxel_feature = self.voxel_conv(torch.cat([voxel_corr.unsqueeze(dim=1), voxel_xyz.permute(0, 3, 1, 2).contiguous()], dim=1))
+        corr = voxel_feature.squeeze(dim=1)
+        corr_feature.append(corr.transpose(1, 2).contiguous())
+        
+        return self.out_conv(torch.cat(corr_feature, dim=1))
 
     def get_knn_feature(self, coords):
         b, n_p, _ = coords.size()
@@ -103,3 +154,13 @@ class CorrBlock(nn.Module):
         corr = torch.matmul(fmap1.transpose(1, 2), fmap2)
         corr = corr / torch.sqrt(torch.tensor(dim).float())
         return corr
+
+    def get_base(self):
+        num = self.resolution ** 3
+        base = torch.zeros((num, 3))
+        for i in range(num):
+            quotient = i
+            for j in range(3):
+                base[i][2-j] = quotient % self.resolution - 1
+                quotient = quotient // self.resolution
+        return base
